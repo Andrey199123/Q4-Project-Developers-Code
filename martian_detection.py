3ba582f1603e5ac94cfcbf4e5df9da262c7f425d
emmachetann <149686629+1002215@users.noreply.github.com>
2025-04-29 08:09:35 -0400

    Update martian_detection.py make it cv2 instead of cv

--- martian_detection.py
+++ martian_detection.py
@@ -14,7 +14,7 @@ https://www.geeksforgeeks.org/python-opencv-cv2-puttext-method/
 
 """
 
-import cv2 as cv
+import cv2
 
 def ORB(frame):
    """Detect and display the Martian on each frame.
@@ -27,16 +27,16 @@ def ORB(frame):
                'WE ARE NOT ALONE!!'
    """
 
-   martian = cv.imread('templates/martian.png', 0)
-   frame_resized = cv.resize(frame, (312,380)) # Resize to dimensions of the martian template for ORB
-   frame_blur = cv.blur(frame_resized, (15,15)) # Reduce noise for less inaccurate matches
+   martian = cv2.imread('templates/martian.png', 0)
+   frame_resized = cv2.resize(frame, (312,380)) # Resize to dimensions of the martian template for ORB
+   frame_blur = cv2.blur(frame_resized, (15,15)) # Reduce noise for less inaccurate matches
 
-   orb = cv.ORB_create()
+   orb = cv2.ORB_create()
 
    keypoints1, descriptors1 = orb.detectAndCompute(martian, None)
    keypoints2, descriptors2 = orb.detectAndCompute(frame_blur, None)
 
-   bf = cv.BFMatcher(cv.NORM_HAMMING, crossCheck=True)
+   bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
    try:
       matches = bf.match(descriptors1,descriptors2)
       matches = sorted(matches, key=lambda x: x.distance)
@@ -62,7 +62,7 @@ def show_not_alone(frame):
                'WE ARE NOT ALONE!!' in the top left corner.
    """
 
-   font = cv.FONT_HERSHEY_SIMPLEX
+   font = cv2.FONT_HERSHEY_SIMPLEX
    font_scale = 1
    color = (255, 255, 255)  # White color
    thickness = 2
@@ -70,20 +70,20 @@ def show_not_alone(frame):
 
    text = "WE ARE NOT ALONE!!"
 
-   text_size = cv.getTextSize(text, font, font_scale, thickness)[0] # Get text size to create a background rectangle
+   text_size = cv2.getTextSize(text, font, font_scale, thickness)[0] # Get text size to create a background rectangle
    text_width, text_height = text_size
 
    rectangle_color = (0, 0, 255)  # Red color
    rectangle_position1 = position  # Top-left corner of the rectangle is the same as the text position
    rectangle_position2 = (position[0] + text_width, position[1] - text_height - 10)  # Bottom-right corner
 
-   cv.rectangle(frame, rectangle_position1, rectangle_position2, rectangle_color, -1)
+   cv2.rectangle(frame, rectangle_position1, rectangle_position2, rectangle_color, -1)
 
-   cv.putText(frame, text, position, font, font_scale, color, thickness, cv.LINE_AA)
+   cv2.putText(frame, text, position, font, font_scale, color, thickness, cv2.LINE_AA)
 
 
 video_path = '/Users/pl1002215/PycharmProjects/object_detection/templates/IMG_5017.mov'
-cap = cv.VideoCapture(video_path)
+cap = cv2.VideoCapture(video_path)
 
 if not cap.isOpened():
    print("Error: Could not open video file.")
@@ -93,10 +93,10 @@ else:
       if not ret:
          break  # Break the loop if no more frames are read
 
-      cv.imshow('Martian Detection PWP', ORB(frame))
+      cv2.imshow('Martian Detection PWP', ORB(frame))
 
-      if cv.waitKey(25) & 0xFF == ord('q'):
+      if cv2.waitKey(25) & 0xFF == ord('q'):
          break  # Break the loop if 'q' is pressed
 
    cap.release()
-   cv.destroyAllWindows()
+   cv2.destroyAllWindows()
