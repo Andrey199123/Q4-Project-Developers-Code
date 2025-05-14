b626dc18c91395fe4f4eed8a2c3042526e0c8fed
emmachetann <149686629+1002215@users.noreply.github.com>
2025-05-14 08:43:12 -0400

    Create obstacle_detection.py w model and example explaining how to implement us sensors

---
+++ obstacle_detection.py
@@ -0,0 +1,3 @@
+'''HC-SR04 Ultrasonic Sensor
+https://thepihut.com/blogs/raspberry-pi-tutorials/hc-sr04-ultrasonic-range-sensor-on-the-raspberry-pi?srsltid=AfmBOoq_EXLQmTaygTmblwEQIBDrtDxaGMp2A-UnRYLbQLzn0q-unkf4
+'''
